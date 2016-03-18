03e6aa2c46ac909a83f9dd02f97804b827da52e1
Rodrigo Recoba <Rodrigo33024@gmail.com>
2016-03-17 21:59:57 -0500

    Index only shows current users documents

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-
+#008 FileCabinet -- Document restrictions visible only to creator  Styleeshets
   resources :docs
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
